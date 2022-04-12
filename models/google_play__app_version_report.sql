1deb845a7c1dac2df4250201fd4ea7efbcdee356
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2022-04-12 16:53:32 -0700

    app version

---
+++ models/google_play__app_version_report.sql
@@ -0,0 +1,146 @@
+with installs as (
+
+    select *
+    from {{ var('stats_installs_app_version') }}
+), 
+
+ratings as (
+
+    select *
+    from {{ var('stats_ratings_app_version') }}
+), 
+
+crashes as (
+
+    select *
+    from {{ var('stats_crashes_app_version') }}
+), 
+
+install_metrics as (
+
+    select
+        *,
+        sum(daily_device_installs) over (partition by app_version_code, package_name rows between unbounded preceding and current row) as total_device_installs,
+        sum(daily_device_uninstalls) over (partition by app_version_code, package_name rows between unbounded preceding and current row) as total_device_uninstalls
+    from installs 
+), 
+
+app_version_join as (
+
+    select 
+        -- these 3 columns are the grain of this model
+        coalesce(install_metrics.date_day, ratings.date_day, crashes.date_day) as date_day,
+        coalesce(install_metrics.app_version_code, ratings.app_version_code, crashes.app_version_code) as app_version_code,
+        coalesce(install_metrics.package_name, ratings.package_name, crashes.package_name) as package_name,
+
+        -- metrics based on unique devices + users
+        coalesce(install_metrics.active_device_installs, 0) as active_device_installs,
+        coalesce(install_metrics.daily_device_installs, 0) as daily_device_installs,
+        coalesce(install_metrics.daily_device_uninstalls, 0) as daily_device_uninstalls,
+        coalesce(install_metrics.daily_device_upgrades, 0) as daily_device_upgrades,
+        coalesce(install_metrics.daily_user_installs, 0) as daily_user_installs,
+        coalesce(install_metrics.daily_user_uninstalls, 0) as daily_user_uninstalls,
+        
+        -- metrics based on events. a user or device can have multiple events in one day
+        coalesce(crashes.daily_crashes, 0) as daily_crashes,
+        coalesce(crashes.daily_anrs, 0) as daily_anrs,
+        coalesce(install_metrics.install_events, 0) as install_events,
+        coalesce(install_metrics.uninstall_events, 0) as uninstall_events,
+        coalesce(install_metrics.update_events, 0) as update_events,    
+
+        -- all of the following fields (except daily_average_rating) are rolling metrics that we'll use window functions to backfill instead of coalescing
+        install_metrics.total_unique_user_installs,
+        install_metrics.total_device_installs,
+        install_metrics.total_device_uninstalls,
+        ratings.daily_average_rating, -- this one actually isn't rolling but we won't coalesce days with no reviews to 0 rating
+        ratings.rolling_total_average_rating
+    from install_metrics
+    full outer join ratings
+        on install_metrics.date_day = ratings.date_day
+        and install_metrics.package_name = ratings.package_name
+        and coalesce(install_metrics.app_version_code, 'null_app_version') = coalesce(ratings.app_version_code, 'null_app_version') -- this really doesn't happen IRL but let's be safe
+    full outer join crashes
+        on install_metrics.date_day = crashes.date_day
+        and install_metrics.package_name = crashes.package_name
+        and coalesce(install_metrics.app_version_codem 'null_app_version') = coalesce(crashes.app_version_code, 'null_app_version')
+), 
+
+-- to backfill in days with NULL values for rolling metrics, we'll create partitions to batch them together with records that have non-null values
+-- we can't just use last_value(ignore nulls) because of postgres :/
+create_partitions as (
+
+    select
+        *
+
+    {%- set rolling_metrics = ['rolling_total_average_rating', 'total_unique_user_installs', 'total_device_installs', 'total_device_uninstalls'] -%}
+
+    {% for metric in rolling_metrics -%}
+        , sum(case when {{ metric }} is null 
+                then 0 else 1 end) over (partition by app_version_code, package_name order by date_day asc rows unbounded preceding) as {{ metric | lower }}_partition
+    {%- endfor %}
+    from app_version_join
+), 
+
+-- now we'll take the non-null value for each partitioned batch and propagate it across the rows included in the batch
+fill_values as (
+
+    select 
+        date_day,
+        app_version_code,
+        package_name,
+        active_device_installs,
+        daily_device_installs,
+        daily_device_uninstalls,
+        daily_device_upgrades,
+        daily_user_installs,
+        daily_user_uninstalls,
+        daily_crashes,
+        daily_anrs,
+        install_events,
+        uninstall_events,
+        update_events,
+        daily_average_rating
+
+        {% for metric in rolling_metrics -%}
+
+        , first_value( {{ metric }} ) over (
+            partition by {{ metric | lower }}_partition, app_version_code, package_name order by date_day asc rows between unbounded preceding and current row) as {{ metric }}
+
+        {%- endfor %}
+    from create_partitions
+), 
+
+final as (
+
+    select 
+        date_day,
+        app_version_code,
+        package_name,
+        active_device_installs,
+        daily_device_installs,
+        daily_device_uninstalls,
+        daily_device_upgrades,
+        daily_user_installs,
+        daily_user_uninstalls,
+        daily_crashes,
+        daily_anrs,
+        install_events,
+        uninstall_events,
+        update_events,
+        daily_average_rating,
+
+        -- leave null if there are no ratings yet
+        rolling_total_average_rating,
+
+        -- the first day will have NULL values, let's make it 0
+        coalesce(total_unique_user_installs, 0) as total_unique_user_installs,
+        coalesce(total_device_installs, 0) as total_device_installs,
+        coalesce(total_device_uninstalls, 0) as total_device_uninstalls,
+
+        -- calculate difference rolling metric
+        coalesce(total_device_installs, 0) - coalesce(total_device_uninstalls, 0) as net_device_installs
+    from fill_values
+)
+
+select *
+from final
