e5156aa09d648241e7d71e7aea376dc3ff7aea62
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2022-04-13 17:09:21 -0700

    overview model

--- models/google_play__overview_report.sql
+++ models/google_play__overview_report.sql
@@ -1,13 +1,19 @@
 with installs as (
 
     select *
-    from {{ var('stats_installs_country') }}
+    from {{ var('stats_installs_overview') }}
 ), 
 
 ratings as (
 
     select *
-    from {{ var('stats_ratings_country') }}
+    from {{ var('stats_ratings_overview') }}
+), 
+
+crashes as (
+
+    select *
+    from {{ var('stats_crashes_overview') }}
 ), 
 
 store_performance as (
@@ -20,27 +26,39 @@ install_metrics as (
 
     select
         *,
-        sum(daily_device_installs) over (partition by country, package_name rows between unbounded preceding and current row) as total_device_installs,
-        sum(daily_device_uninstalls) over (partition by country, package_name rows between unbounded preceding and current row) as total_device_uninstalls
+        sum(daily_device_installs) over (partition by package_name rows between unbounded preceding and current row) as total_device_installs,
+        sum(daily_device_uninstalls) over (partition by package_name rows between unbounded preceding and current row) as total_device_uninstalls
     from installs 
 ), 
+
+-- this is at the country level so let's roll up to overview
+store_performance_rollup as (
+
+    select 
+        date_day,
+        package_name,
+        sum(store_listing_acquisitions) as store_listing_acquisitions,
+        sum(store_listing_visitors) as store_listing_visitors
+    from store_performance
+    group by 1,2
+),
 
 store_performance_metrics as (
 
     select
         *,
-        sum(store_listing_acquisitions) over (partition by country_region, package_name rows between unbounded preceding and current row) as total_store_acquisitions,
-        sum(store_listing_visitors) over (partition by country_region, package_name rows between unbounded preceding and current row) as total_store_visitors
-    from store_performance
+        round(store_listing_acquisitions * 1.0 / nullif(store_listing_visitors, 0), 4) as store_listing_conversion_rate,
+        sum(store_listing_acquisitions) over (partition by package_name rows between unbounded preceding and current row) as total_store_acquisitions,
+        sum(store_listing_visitors) over (partition by package_name rows between unbounded preceding and current row) as total_store_visitors
+    from store_performance_rollup
 ), 
 
-country_join as (
+overview_join as (
 
     select 
-        -- these 3 columns are the grain of this model
-        coalesce(install_metrics.date_day, ratings.date_day, store_performance_metrics.date_day) as date_day,
-        coalesce(install_metrics.country, ratings.country, store_performance_metrics.country_region) as country,
-        coalesce(install_metrics.package_name, ratings.package_name, store_performance_metrics.package_name) as package_name,
+        -- these 2 columns are the grain of this model
+        coalesce(install_metrics.date_day, ratings.date_day, store_performance_metrics.date_day, crashes.date_day) as date_day,
+        coalesce(install_metrics.package_name, ratings.package_name, store_performance_metrics.package_name, crashes.package_name) as package_name,
 
         -- metrics based on unique devices + users
         coalesce(install_metrics.active_device_installs, 0) as active_device_installs,
@@ -52,31 +70,34 @@ country_join as (
         coalesce(store_performance_metrics.store_listing_acquisitions) as store_listing_acquisitions,
         coalesce(store_performance_metrics.store_listing_visitors) as store_listing_visitors,
         store_performance_metrics.store_listing_conversion_rate, -- not coalescing if there aren't any visitors 
-        
+
         -- metrics based on events. a user or device can have multiple installs in one day
+        coalesce(crashes.daily_crashes, 0) as daily_crashes,
+        coalesce(crashes.daily_anrs, 0) as daily_anrs,
         coalesce(install_metrics.install_events, 0) as install_events,
         coalesce(install_metrics.uninstall_events, 0) as uninstall_events,
         coalesce(install_metrics.update_events, 0) as update_events,    
 
-        -- all of the following fields (except %'s') are rolling metrics that we'll use window functions to backfill instead of coalescing
+        -- all of the following fields (except daily_average_rating) are rolling metrics that we'll use window functions to backfill instead of coalescing
         install_metrics.total_unique_user_installs,
         install_metrics.total_device_installs,
         install_metrics.total_device_uninstalls,
-        ratings.daily_average_rating, -- this one actually isn't rolling but we won't coalesce days with no reviews to 0 rating
+        ratings.daily_average_rating, -- this one actually isn't rolling but we won't coalesce days with no reviews to 0 rating. todo: move
         ratings.rolling_total_average_rating,
         store_performance_metrics.total_store_acquisitions,
         store_performance_metrics.total_store_visitors
-        
+
     from install_metrics
     full outer join ratings
         on install_metrics.date_day = ratings.date_day
         and install_metrics.package_name = ratings.package_name
-        and coalesce(install_metrics.country, 'null_country') = coalesce(ratings.country, 'null_country') -- in the source package we aggregate all null country records together into one batch per day
     full outer join store_performance_metrics
         on store_performance_metrics.date_day = install_metrics.date_day
         and store_performance_metrics.package_name = install_metrics.package_name
-        and coalesce(store_performance_metrics.country_region, 'null_country') = coalesce(install_metrics.country, 'null_country')
-), 
+    full outer join crashes
+        on install_metrics.date_day = crashes.date_day
+        and install_metrics.package_name = crashes.package_name
+),
 
 -- to backfill in days with NULL values for rolling metrics, we'll create partitions to batch them together with records that have non-null values
 -- we can't just use last_value(ignore nulls) because of postgres :/
@@ -89,9 +110,9 @@ create_partitions as (
 
     {% for metric in rolling_metrics -%}
         , sum(case when {{ metric }} is null 
-                then 0 else 1 end) over (partition by country, package_name order by date_day asc rows unbounded preceding) as {{ metric | lower }}_partition
+                then 0 else 1 end) over (partition by package_name order by date_day asc rows unbounded preceding) as {{ metric | lower }}_partition
     {%- endfor %}
-    from country_join
+    from overview_join
 ), 
 
 -- now we'll take the non-null value for each partitioned batch and propagate it across the rows included in the batch
@@ -99,7 +120,6 @@ fill_values as (
 
     select 
         date_day,
-        country,
         package_name,
         active_device_installs,
         daily_device_installs,
@@ -107,6 +127,8 @@ fill_values as (
         daily_device_upgrades,
         daily_user_installs,
         daily_user_uninstalls,
+        daily_crashes,
+        daily_anrs,
         install_events,
         uninstall_events,
         update_events,
@@ -118,7 +140,7 @@ fill_values as (
         {% for metric in rolling_metrics -%}
 
         , first_value( {{ metric }} ) over (
-            partition by {{ metric | lower }}_partition, country, package_name order by date_day asc rows between unbounded preceding and current row) as {{ metric }}
+            partition by {{ metric | lower }}_partition, package_name order by date_day asc rows between unbounded preceding and current row) as {{ metric }}
 
         {%- endfor %}
     from create_partitions
@@ -128,7 +150,6 @@ final as (
 
     select 
         date_day,
-        country,
         package_name,
         active_device_installs,
         daily_device_installs,
@@ -136,12 +157,14 @@ final as (
         daily_device_upgrades,
         daily_user_installs,
         daily_user_uninstalls,
+        daily_crashes,
+        daily_anrs,
         install_events,
         uninstall_events,
         update_events,
-        store_listing_acquisitions,
+        store_listing_acquisitions, -- should we prepend with daily_?
         store_listing_visitors,
-        store_listing_conversion_rate,
+        store_listing_conversion_rate, -- daily
         daily_average_rating,
 
         -- leave null if there are no ratings yet
